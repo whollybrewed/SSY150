41e689e0226b0ff3813bba61c84522d71124dd9f
whollybrewed <andylee47695@gmail.com>
2020-04-16 00:31:26 +0200

    indent

--- LAB1/waterfall.m
+++ LAB1/waterfall.m
@@ -8,13 +8,13 @@ ws=100; % block/frame size (10ms->100samples/frame)
 L=floor(length(y)/ws);
 figure, hold;
 for k=440:460
-seg=y(1+(k-1)*ws:k*ws);
-N=length(seg);
-nfft=10*N;
-wseg=seg.*hamming(N);
-pseg=[wseg;zeros(nfft,1)];
-cep=abs(ifft(log(abs(fft(pseg,nfft)))));
-cep(1)=0;
-c2=cep+k*1; 
-plot(c2(1:nfft/2));
+    seg=y(1+(k-1)*ws:k*ws);
+    N=length(seg);
+    nfft=10*N;
+    wseg=seg.*hamming(N);
+    pseg=[wseg;zeros(nfft,1)];
+    cep=abs(ifft(log(abs(fft(pseg,nfft)))));
+    cep(1)=0;
+    c2=cep+k*1; 
+    plot(c2(1:nfft/2));
 end
