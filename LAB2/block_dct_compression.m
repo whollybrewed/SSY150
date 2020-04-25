3f59dcc8d08f9dc8a748d2df64a80c4e4d954a1c
whollybrewed <andylee47695@gmail.com>
2020-04-25 19:46:26 +0200

    add code for Task2

---
+++ LAB2/block_dct_compression.m
@@ -0,0 +1,43 @@
+% code for Task 2
+clear all; close all;
+clc
+
+inputFile='Trees1.avi';
+outputFile='frm10.bmp';
+% compression ratio
+CR=0.98;
+[mov,frm]=aviread(inputFile);
+% convert frame into image
+frmIdx=10;
+iRGB=frame2im(mov(frmIdx));
+% RGB to grayscale
+iGray=rgb2gray(iRGB);
+% Normalization, [0 255] -> [0 1]
+iRef=mat2gray(iGray);
+% ==================function handle==================
+% block-based DCT
+dctblock=@(block_struct)dct2(block_struct.data);
+% block-based DCT followed by mask applied 
+dctTh=@(block_struct)dctmask(dct2(block_struct.data),CR);
+% inverse block-based DCT
+invdct=@(block_struct)idct2(block_struct.data);
+% ===============process block by block===============
+% temp saved for plotting
+jTemp=blockproc(iRef,[8 8],dctblock);
+j=blockproc(iRef,[8 8],dctTh);
+iCmpr = blockproc(j,[8 8],invdct);
+
+e=abs(iRef-iCmpr);
+peaksnr=psnr(iCmpr,iRef);
+MSSIM=ssim(iCmpr,iRef);
+%===================plotting==========================
+figure, colormap gray
+subplot(2,2,1);
+imshow(iRef), title('Original image'); 
+subplot(2,2,2);
+imshow(log(abs(jTemp)),[]), title('logscale DCT'); 
+subplot(2,2,3);
+imshow(iCmpr), title('Compressed image'); 
+subplot(2,2,4);
+imshow(e*30), title('error * 30');
+
